4425f4098528e2363d56bd1302a76367913afe91
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-01-31 04:31:10 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,25 @@
+class Artist
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(name, genre)
+    Song.new(self, name, genre)
+  end
+
+  def songs
+    Song.all.select do |artist|
+      song.artist == self
+    end
+  end
+
+end
